558c7cc175cdcebe30154441da175e2b16c0133c
banishangural <anguralbanish2@gmail.com>
2021-03-21 21:03:01 -0500

    Created the Repo

---
+++ ALERT AI/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace ALERT_AI
+{
+    partial class Alert_AI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Speak_Button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Speak_Button
+            // 
+            this.Speak_Button.BackColor = System.Drawing.Color.Red;
+            this.Speak_Button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Speak_Button.Location = new System.Drawing.Point(120, 339);
+            this.Speak_Button.Name = "Speak_Button";
+            this.Speak_Button.Size = new System.Drawing.Size(130, 52);
+            this.Speak_Button.TabIndex = 1;
+            this.Speak_Button.Text = "Activate";
+            this.Speak_Button.UseVisualStyleBackColor = false;
+            this.Speak_Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::ALERT_AI.Properties.Resources.alberta;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(308, 321);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Alert_AI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Speak_Button);
+            this.Name = "Alert_AI";
+            this.Text = "ALERT AI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Speak_Button;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ALERT AI/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.IO;
+using System.Media;
+using System.Diagnostics;
+using System.Net;
+using static ALERT_AI.WeatherClass;
+using Newtonsoft.Json;
+
+namespace ALERT_AI
+{
+    public partial class Alert_AI : Form
+    {
+        public bool wake = false;
+        public string owner = "banish";
+        public Alert_AI()
+        {
+            InitializeComponent();
+        }
+
+        SpeechSynthesizer aiSpeech = new SpeechSynthesizer();
+        SpeechRecognitionEngine aiRecognise = new SpeechRecognitionEngine();
+        PromptBuilder aiPrompt = new PromptBuilder();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Speak_Button.Text = "Activated";
+            aiSpeech.SelectVoiceByHints(VoiceGender.Female);
+            aiSpeech.Speak("hi, i am alert ai. if you need me, just say \"hey alert\".");
+            Choices commandList = new Choices();
+            commandList.Add(File.ReadAllLines(@"C:\ALERT AI\commands.txt"));
+
+            Grammar grammarObject = new Grammar(new GrammarBuilder(commandList));
+
+            try
+            {
+                aiRecognise.RequestRecognizerUpdate();
+                aiRecognise.LoadGrammar(grammarObject);
+                aiRecognise.SpeechRecognized += aiRecognise_SpeechRecognized;
+                aiRecognise.SetInputToDefaultAudioDevice();
+                aiRecognise.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch
+            {
+
+                return;
+            }
+            aiPrompt.ClearContent();
+            aiSpeech.Speak(aiPrompt);
+            
+        }
+
+        public void Say(string sentence)
+        {
+            aiSpeech.SpeakAsync(sentence);
+            wake = false;
+        }
+
+        private void aiRecognise_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speechSaid = e.Result.Text;
+            string apiKey = "4603d9d14eba355f4b70d7802bb16587";
+
+            if (speechSaid == "hey alert" || speechSaid == "hi alert")
+            {
+                SoundPlayer player = new SoundPlayer(@"C:\ALERT AI\Sounds\WakeUp.wav");
+                player.Play();
+                wake = true;
+                
+            }
+
+            if (wake)
+            {
+                switch (speechSaid)
+                {
+                    case ("hello"):
+                        Say("hi banish");
+                        break;
+
+                    case ("how are you"):
+                        Say("I am doing pretty good" + owner);
+                        break;
+
+                    case ("what is my name"):
+                        Say("your name is " + owner);
+                        break;
+
+                    case ("open google"):
+                        Say("opening google");
+                        Process.Start("https://www.google.com");
+                        break;
+
+                    case ("close"):
+                        Say("closing program");
+                        SendKeys.Send("%{F4}");
+                        break;
+
+                    case ("exit"):
+                        aiSpeech.Speak("closing down banish, thanks for using alert ai");
+                        Application.Exit();
+                        break;
+
+                    case ("what is the time right now"):
+                    Say("the time is" + DateTime.Now.ToString("t"));
+                        break;
+
+                    case ("what is the weather today"):
+                        HttpWebRequest apiRequest = WebRequest.Create("https://api.openweathermap.org/data/2.5/weather?q=edmonton&appid=" + apiKey + "&units=metric") as HttpWebRequest;
+                        string apiResponse = "";
+                        using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
+                        {
+                            
+                            StreamReader reader = new StreamReader(response.GetResponseStream());
+                            apiResponse = reader.ReadToEnd();
+                        }
+
+                        ResponseWeather rootObject = JsonConvert.DeserializeObject<ResponseWeather>(apiResponse);
+                        Say("Today's weather is " + rootObject.weather[0].main + " and the temperature is " + rootObject.main.temp + " degree Celsius.");
+                        break;
+
+                        
+                }
+            }
+
+
+        }
+    }
+}
